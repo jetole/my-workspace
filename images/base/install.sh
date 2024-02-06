6ffea86c93b5f3a5c04203d3351ee3aabbbe87a6
Joseph Tole <Joseph.Tole@gmail.com>
2024-02-05 23:03:32 -0500

    Fixed: Added missing command

--- images/base/install.sh
+++ images/base/install.sh
@@ -54,4 +54,5 @@ apt-get install -y google-cloud-cli
   install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
   echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list
   rm -fv packages.microsoft.gpg
+  apt-get install -y code
 )
