064d87d84ca86b01bd6c40c019d7a6271f522d10
Joseph Tole <Joseph.Tole@gmail.com>
2024-02-05 22:43:26 -0500

    Added more packages

--- images/base/install.sh
+++ images/base/install.sh
@@ -24,3 +24,34 @@ for x in \
 do
   ln -fs "/usr/bin/distrobox-host-exec" "/usr/local/bin/${x}"
 done
+
+
+# Insall AWS CLI
+(
+  cd /tmp
+  curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+  unzip awscliv2.zip
+  ./aws/install
+  rm -fv awscliv2.zip aws
+)
+
+# Install Azure CLI
+curl -sL https://aka.ms/InstallAzureCLIDeb | bash
+
+# Install Google Cloud SDK
+echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | \
+  tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | \
+  apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
+apt-get update
+apt-get install -y google-cloud-cli
+
+# Install vscode
+(
+  cd "$(mktemp -d)" || true
+  wget -qO- https://packages.microsoft.com/keys/microsoft.asc | \
+    gpg --dearmor > packages.microsoft.gpg
+  install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+  echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list
+  rm -fv packages.microsoft.gpg
+)
