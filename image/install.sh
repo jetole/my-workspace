550f06834676aabaadd6943b7f21273f950f2a44
Joseph Tole <Joseph.Tole@gmail.com>
2024-02-07 08:49:59 -0500

    Commented out vscode install

--- image/install.sh
+++ image/install.sh
@@ -46,14 +46,14 @@ curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | \
 apt-get update
 apt-get install -y google-cloud-cli
 
-# Install vscode
-(
-  cd "$(mktemp -d)" || true
-  wget -qO- https://packages.microsoft.com/keys/microsoft.asc | \
-    gpg --dearmor > packages.microsoft.gpg
-  install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
-  echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list
-  rm -fv packages.microsoft.gpg
-  apt-get update
-  apt-get install -y code
-)
+# # Install vscode
+# (
+#   cd "$(mktemp -d)" || true
+#   wget -qO- https://packages.microsoft.com/keys/microsoft.asc | \
+#     gpg --dearmor > packages.microsoft.gpg
+#   install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+#   echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list
+#   rm -fv packages.microsoft.gpg
+#   apt-get update
+#   apt-get install -y code
+# )
