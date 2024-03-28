955cceac9f045002588700f26b45579cd3e55414
Joseph Tole <Joseph.Tole@gmail.com>
2024-03-27 22:03:32 -0400

    Removed packages and added asdf

--- packages.sh
+++ packages.sh
@@ -20,28 +20,7 @@ mkdir -p /opt/zsh/oh-my-zsh-custom/{plugins,themes}
 )
 
 # Create links to host apps
-for x in \
-  podman
-do
-  ln -fs "/usr/bin/distrobox-host-exec" "/usr/local/bin/${x}"
-done
+ln -fs "/usr/bin/distrobox-host-exec" "/usr/local/bin/${x}"
 
-# Insall AWS CLI
-(
-  cd /tmp
-  curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-  unzip awscliv2.zip
-  ./aws/install
-  rm -fv awscliv2.zip aws
-)
-
-# Install Azure CLI
-curl -sL https://aka.ms/InstallAzureCLIDeb | bash
-
-# Install Google Cloud SDK
-echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | \
-  tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
-curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | \
-  apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
-apt-get update
-apt-get install -y google-cloud-cli
+# Install asdf
+git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.14.0
